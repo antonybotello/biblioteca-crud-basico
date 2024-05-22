f5357f7b397637a6245eda1e473c342b5a2036ae
Antony Botello <antonybotello@gmail.com>
2024-05-22 09:41:12 -0500

    first commit

---
+++ src/main/java/com/usta/ListarController.java
@@ -0,0 +1,25 @@
+package com.usta;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ListarController {
+
+
+    @FXML
+    private void switchToAgregar() throws IOException {
+        App.setRoot("agregar");
+    }
+    @FXML
+    private void switchToListar() throws IOException {
+        App.setRoot("listar");
+    }
+    @FXML
+    private void switchToEditar() throws IOException {
+        App.setRoot("editar");
+    }
+    @FXML
+    private void switchToEliminar() throws IOException {
+        App.setRoot("eliminar");
+    }
+}
